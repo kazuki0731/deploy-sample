fee99533d6636545ddd336a3337623a3ba6cb140
kawabata <kazuki.kawabata02@gmail.com>
2021-09-03 03:16:21 +0900

    init

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/api", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
+
+
+module.exports = router;
