aa8256a218510dfbaca594b8f5cca119f276facc
kawabata <kazuki.kawabata02@gmail.com>
2021-09-03 04:15:25 +0900

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
 
 function App() {
   const [message, setMessage] = useState("");
-  const [name, setName] = useState([]);
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
-
-  const getUser = () => {
-    axios.get("/user").then((res) => {
-      name.push(res.data.name);
-      setName([...name]);
-    });
-  };
-
   return (
     <div className="App">
       <h1>フロントエンド</h1>
       <p>{message}</p>
-      <button onClick={getUser}>表示</button>
-      <p>{name}</p>
     </div>
   );
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const sqlite3 = require("sqlite3");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -11,17 +10,9 @@ router.get("/api", (req, res) => {
   res.json({ message: "Hello World and Japan!" });
 });
 
-router.get("/user", (req, res) => {
-  const db = new sqlite3.Database("../mydb.sqlite3");
-  db.get("select * from users", (err, row) => {
-    res.json(row);
-    console.log(row);
-  });
-  db.close();
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
 });
 
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 module.exports = router;
