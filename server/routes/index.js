8749a175c0c63ace0a23ed91ae970b5dee1eabd0
kawabata <kazuki.kawabata02@gmail.com>
2021-09-03 04:00:08 +0900

    third

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const sqlite3 = require("sqlite3");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -10,9 +11,17 @@ router.get("/api", (req, res) => {
   res.json({ message: "Hello World and Japan!" });
 });
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+router.get("/user", (req, res) => {
+  const db = new sqlite3.Database("../mydb.sqlite3");
+  db.get("select * from users", (err, row) => {
+    res.json(row);
+    console.log(row);
+  });
+  db.close();
 });
 
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
