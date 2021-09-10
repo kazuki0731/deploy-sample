3f95eba17d1fefd718f2e1018164f9bf997a2f96
kawabata <kazuki.kawabata02@gmail.com>
2021-09-10 15:29:00 +0900

    favicon

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const pool = require("../db/pool.js");
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("OK");
 });
